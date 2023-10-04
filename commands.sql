4c4b0d6b32b2e34ef1fff2b4b37b8a75d7c98a5f
Kristian Hannula <kristian.hannula@proton.me>
2023-10-04 10:42:28 +0300

    Task 13.2 done

---
+++ commands.sql
@@ -0,0 +1,5 @@
+insert into blogs (author, url, title) values ('Kristian', 'https://www.kristianhannula.com', 'Kristians hom
+e');
+
+insert into blogs (author, url, title) values ('Osmo', 'https://www.soininvaara.fi', 'Soininvaara.fi');
+
