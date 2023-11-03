3926701280d4ab195ebda05d99881f74bd2ba75d
Kristian <kristian.hannula@proton.me>
2023-11-03 10:41:08 +0200

    Task 13.6 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,6 +30,16 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 router.delete('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
     await req.blog.destroy()
